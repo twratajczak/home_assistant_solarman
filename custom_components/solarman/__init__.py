20337d4b4cb2a0d439bd2211c9a6b1ed89c71ddc
Tomasz Ratajczak <twratajczak@gmail.com>
2025-01-06 13:35:07 +0100

    fix unload
    remove invalid unload handler

--- custom_components/solarman/__init__.py
+++ custom_components/solarman/__init__.py
@@ -16,7 +16,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Solarman Collector from a config entry."""
     _LOGGER.debug(f'__init__.py:async_setup_entry({entry.as_dict()})')
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(update_listener))
     return True
 
 
@@ -24,13 +23,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     _LOGGER.debug(f'__init__.py:async_unload_entry({entry.as_dict()})')
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    _LOGGER.debug(f'__init__.py:update_listener({entry.as_dict()})')
-    hass.data[DOMAIN][entry.entry_id].config(entry)
-    entry.title = entry.options[CONF_NAME]
